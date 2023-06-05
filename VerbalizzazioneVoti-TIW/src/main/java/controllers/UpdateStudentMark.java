5c83ba4e775c4ebfdbadf1f3f358ef3a8720e104
LucaTosetti01 <luca1.tosetti@mail.polimi.it>
2023-06-06 00:04:04 +0200

    Inizio documentazione
    Scritta la documentazione della traccia, completamento specifiche, analisi requisiti DB, schema ER (da modificare), schema DB

--- VerbalizzazioneVoti-TIW/src/main/java/controllers/UpdateStudentMark.java
+++ VerbalizzazioneVoti-TIW/src/main/java/controllers/UpdateStudentMark.java
@@ -60,6 +60,11 @@ public class UpdateStudentMark extends HttpServlet {
 			// Checking if the format of the mark retrieved from the request is correct
 			ceDAO.checkIfMarkFormatIsCorrect(newMark);
 
+			/*TODO: Provare ad implementare una logica che permetta comunque di
+			 * 		inserire come voto "", ma in quel caso riportare lo stato del
+			 * 		voto a "Non inserito"
+			 * */
+			 
 			// Ignoring the update if the sent mark is equals to ""
 			if (!newMark.equals("")) {
 				// Proceeding to update student mark with the new value
